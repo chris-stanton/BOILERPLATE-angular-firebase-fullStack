59c2e54713ff52c658297dddceef88af1960218d
Chris Stanton <cstanton0760@yahoo.com>
2017-05-01 13:13:41 -0500

    first commit

---
+++ public/scripts/config.js
@@ -0,0 +1,42 @@
+/*
+
+########  ######## ########  ##          ###     ######  ########
+##     ## ##       ##     ## ##         ## ##   ##    ## ##
+##     ## ##       ##     ## ##        ##   ##  ##       ##
+########  ######   ########  ##       ##     ## ##       ######
+##   ##   ##       ##        ##       ######### ##       ##
+##    ##  ##       ##        ##       ##     ## ##    ## ##
+##     ## ######## ##        ######## ##     ##  ######  ########
+
+######## ##     ## ####  ######
+   ##    ##     ##  ##  ##    ##
+   ##    ##     ##  ##  ##
+   ##    #########  ##   ######
+   ##    ##     ##  ##        ##
+   ##    ##     ##  ##  ##    ##
+   ##    ##     ## ####  ######
+
+   ######## #### ##       ########
+   ##        ##  ##       ##
+   ##        ##  ##       ##
+   ######    ##  ##       ######
+   ##        ##  ##       ##
+   ##        ##  ##       ##
+   ##       #### ######## ########
+
+*/
+
+// You will need to update these with your own values
+// Do not copy the script tags into here, just the config part
+
+/////////////////////////////////////////////////////////
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCsUEG6gSNl9GSoG5QITmbgn6pWqvUM5jI",
+    authDomain: "fir-template-ff32f.firebaseapp.com",
+    databaseURL: "https://fir-template-ff32f.firebaseio.com",
+    projectId: "fir-template-ff32f",
+    storageBucket: "fir-template-ff32f.appspot.com",
+    messagingSenderId: "751246878615"
+  };
+  firebase.initializeApp(config);
